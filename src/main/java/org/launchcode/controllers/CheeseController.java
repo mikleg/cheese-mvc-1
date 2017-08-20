ceba6f4cf0b44e14995998e02b25e318b95654cd
Mikle Gorbunov <mihailgorbunov74@gmail.com>
2017-08-20 15:31:52 -0700

    added edit fuct-ty. Redirect dosen't work

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -6,10 +6,7 @@ import org.launchcode.models.CheeseType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -66,6 +63,21 @@ public class CheeseController {
             CheeseData.remove(cheeseId);
         }
 
+        return "redirect:";
+    }
+    @RequestMapping(value = "edit/{cheeseId}", method = RequestMethod.GET)
+    public String displayEditForm(Model model, @PathVariable int cheeseId){
+        /*model.addAttribute("title", "Edit Cheese");*/
+        model.addAttribute("cheese", CheeseData.getById(cheeseId));
+        return "cheese/edit";
+    }
+
+    @RequestMapping(value = "edit/{cheeseId}", method = RequestMethod.POST)
+    //@RequestMapping(value = "{cheeseId}", method = RequestMethod.POST)
+    public String processEditForm(int cheeseId, String name, String description){
+        Cheese aCheese = CheeseData.getById(cheeseId);
+        aCheese.setName(name);
+        aCheese.setDescription(description);
         return "redirect:";
     }
 
